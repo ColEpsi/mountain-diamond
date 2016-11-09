2007ff71419c6a888beb377e634977c3cd77d1e2
blazh24 <b.milar@gmail.com>
2016-11-09 23:31:19 +0100

    commit from desktop

--- js/game.js
+++ js/game.js
@@ -6,8 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 }, false);
 
-var current_frame = 0; // frame counter. might prove useful.
-
 var randomNumber = function (min, max) {
   if (min == max) {
               return (min);
@@ -16,14 +14,12 @@ var randomNumber = function (min, max) {
           return ((random * (max - min)) + min);
 };
 
-function jump(){
-    
-}
-
 function startGame(){
   var canvas = document.getElementById("renderCanvas");
   // Load the BABYLON 3D engine
   var engine = new BABYLON.Engine(canvas, true);
+
+
 
   // -------------------------------------------------------------
   // Here begins a function that we will 'call' just after it's built
@@ -107,7 +103,7 @@ function startGame(){
       return tree;
 
   };
-//-----------------------------KONEC TREE GENERATORJA--------------------------------------
+//------------------------------------------------------------------------------------
 
   var createScene = function() {
     // scene objekt
@@ -125,18 +121,20 @@ function startGame(){
     var light1 = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(-1, 1, 0), scene);
 
     // import mesh
-    var dia; 
-    BABYLON.SceneLoader.ImportMesh("", "assets/", "diaMontiffe.babylon", scene, function (newMeshes) {
-    // Set the target of the camera to dia
-    dia = newMeshes[0];
-    camera.target = dia;
-    dia.scaling.x = 0.5;
-    dia.scaling.y = 0.5;
-    dia.scaling.z = 0.5;
+    var mesh; 
+    BABYLON.SceneLoader.ImportMesh("", "", "assets/diaMontiffe.babylon", scene, function (newMeshes) {
+    
+    // Set the target of the camera to the first imported mesh
+    camera.target = newMeshes[0];
+    mesh = newMeshes[0];
+    mesh.scaling.x = 0.5;
+    mesh.scaling.y = 0.5;
+    mesh.scaling.z = 0.5;
+
 
     //WORK IN PROGRESS
     /*
-     if (dia.intersectsMesh(rock, false)) {
+     if (mesh.intersectsMesh(rock, false)) {
       console.log("COLISSION");
     } 
     */
@@ -161,18 +159,20 @@ function startGame(){
     });
     animation2Mesh.setKeys(keys);
   
-    scene.beginDirectAnimation(dia, [animation2Mesh], 0, 400, true);
+    scene.beginDirectAnimation(mesh, [animation2Mesh], 0, 400, true);
     });
 
     var rock;
-    BABYLON.SceneLoader.ImportMesh("", "assets/", "rock.babylon", scene, function (newMeshes) {
+    BABYLON.SceneLoader.ImportMesh("", "", "assets/rock.babylon", scene, function (newMeshes) {
+
+    newMeshes[0].scaling.x = 10;
+    newMeshes[0].scaling.y = 10;
+    newMeshes[0].scaling.z = 10;
+    newMeshes[0].position.z = -800;
     rock = newMeshes[0];
-    rock.scaling.x = 10;
-    rock.scaling.y = 10;
-    rock.scaling.z = 10;
-    rock.position.z = -800;
     });
 
+    
 
     var leafMaterial = new BABYLON.StandardMaterial("leafMaterial", scene);
     leafMaterial.diffuseColor = new BABYLON.Color3(0.5, 1, 0.5);
@@ -185,7 +185,7 @@ function startGame(){
     ground.rotation.x = glMatrix.toRadian(90);
     ground.scaling.y = 10;
     var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
-    groundMaterial.diffuseTexture = new BABYLON.Texture("./textures/snow2.jpg", scene);
+    groundMaterial.diffuseTexture = new BABYLON.Texture("./textures/field-of-dimpled-snow.jpg", scene);
     ground.material = groundMaterial;
 
     var background = BABYLON.Mesh.CreatePlane("Plane", 100, scene);
@@ -217,28 +217,20 @@ function startGame(){
     window.addEventListener('keydown', function(event) {
       switch (event.keyCode) {
         case 37: // Left
-          if(dia.position.x > -90){
-            dia.position.x -= 4;
-            dia.rotation.y = glMatrix.toRadian(-10);
-            window.addEventListener('keyup', function(event) {
-              dia.rotation.y = glMatrix.toRadian(0);
-            });
+          if(mesh.position.x > -90){
+            mesh.position.x -= 4;
+            mesh.rotation.y = glMatrix.toRadian(-10);
           }
           break;
 
         case 38: // Up
-        if(dia.position.y < 5){
-          dia.position.y += 5;
-        }
+          mesh.rotation.y = glMatrix.toRadian(0);
           break;
 
         case 39: // Right
-          if(dia.position.x < 90){
-            dia.position.x += 4;
-            dia.rotation.y = glMatrix.toRadian(10);
-            window.addEventListener('keyup', function(event) {
-              dia.rotation.y = glMatrix.toRadian(0);
-            });
+          if(mesh.position.x < 90){
+            mesh.position.x += 4;
+            mesh.rotation.y = glMatrix.toRadian(10);
           }
           break;
 
@@ -253,7 +245,6 @@ function startGame(){
   var scene = createScene();
   engine.runRenderLoop(function(){
     scene.render();
-    current_frame++;
   });
 
   // Watch for browser/canvas resize events
@@ -262,5 +253,3 @@ function startGame(){
   });
 
 }
-
-

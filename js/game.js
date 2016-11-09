4f03b2bc2081152609ac44b795ebdd524742f109
Blaž Milar <blaz.milar@gmail.com>
2016-11-09 23:10:38 +0100

    1
    1

--- js/game.js
+++ js/game.js
@@ -6,37 +6,29 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 }, false);
 
+var current_frame = 0; // frame counter. might prove useful.
+
+var randomNumber = function (min, max) {
+  if (min == max) {
+              return (min);
+          }
+          var random = Math.random();
+          return ((random * (max - min)) + min);
+};
+
+function jump(){
+    
+}
+
 function startGame(){
   var canvas = document.getElementById("renderCanvas");
   // Load the BABYLON 3D engine
   var engine = new BABYLON.Engine(canvas, true);
+
   // -------------------------------------------------------------
   // Here begins a function that we will 'call' just after it's built
-  var createScene = function() {
-    // scene objekt
-    var scene = new BABYLON.Scene(engine);
-    //barva scene
-    scene.clearColor = new BABYLON.Color3( .5, .5, .5);
-
-    // camera
-    var camera = new BABYLON.ArcRotateCamera("camera1",  0, 0, 0, new BABYLON.Vector3(0, 0, -0), scene);
-    camera.setPosition(new BABYLON.Vector3(0, 25, -60));
-    camera.attachControl(canvas, true);
-    //globalna luc
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 0.5, 0), scene);
-    light.intensity = 0.7;
-    //spot luci
-    var spot = new BABYLON.SpotLight("spot", new BABYLON.Vector3(25, 15, -50), new BABYLON.Vector3(-1, -0.8, 1), 15, 1, scene);
-    spot.diffuse = new BABYLON.Color3(0.545098, 0.270588, 0.0745098);
-    spot.specular = new BABYLON.Color3(0, 0, 0);
-    spot.intensity = 0.8;
-
-    var leafMaterial = new BABYLON.StandardMaterial("leafMaterial", scene);
-    leafMaterial.diffuseColor = new BABYLON.Color3(0.5, 1, 0.5);
-    
-    var woodMaterial = new BABYLON.StandardMaterial(name, scene);
-    
-   QuickTreeGenerator = function(sizeBranch, sizeTrunk, radius, trunkMaterial, leafMaterial, scene) {
+  
+  QuickTreeGenerator = function(sizeBranch, sizeTrunk, radius, trunkMaterial, leafMaterial, scene) {
 
       var tree = new BABYLON.Mesh("tree", scene);
       tree.isVisible = false;
@@ -79,13 +71,6 @@ function startGame(){
           }
 
       }
-      var randomNumber = function (min, max) {
-          if (min == max) {
-              return (min);
-          }
-          var random = Math.random();
-          return ((random * (max - min)) + min);
-      };
 
       map.forEach(function(array) {
           var index, min = -sizeBranch/10, max = sizeBranch/10;
@@ -100,7 +85,6 @@ function startGame(){
               positions[i+2] += rz;
           }
       });
-
       leaves.setVerticesData(BABYLON.VertexBuffer.PositionKind, positions);
       var normals = [];
       BABYLON.VertexData.ComputeNormals(positions, indices, normals);
@@ -123,30 +107,154 @@ function startGame(){
       return tree;
 
   };
-  	var plane = BABYLON.Mesh.CreatePlane("Plane", 100, scene);
-  	plane.rotation.x = 1.57;
+//-----------------------------KONEC TREE GENERATORJA--------------------------------------
 
-  	var tree1 = QuickTreeGenerator(15, 10, 5, woodMaterial, leafMaterial, scene);
-    	tree1.position.x = 20;
+  var createScene = function() {
+    // scene objekt
+    var scene = new BABYLON.Scene(engine);
+    //barva scene
+    scene.clearColor = new BABYLON.Color3( .5, .5, .5);
 
-    	var tree2 = QuickTreeGenerator(15, 10, 5, woodMaterial, leafMaterial, scene);
-    	tree2.position.x = -20; 
+    // camera
+    var camera = new BABYLON.ArcRotateCamera("camera1",  0, 0, 0, new BABYLON.Vector3(0, 0, 0), scene);
+    camera.setPosition(new BABYLON.Vector3(0, 25, -60));
+    //camera.attachControl(canvas, true);
+    //globalna luc
 
-    	var tree3 = QuickTreeGenerator(15, 10, 5, woodMaterial, leafMaterial, scene);
-    	tree3.position.z = 20;  
+    var light0 = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(1, 1, 0), scene);
+    var light1 = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(-1, 1, 0), scene);
 
-    	var tree4 = QuickTreeGenerator(15, 10, 5, woodMaterial, leafMaterial, scene);
-    	tree4.position.x = 25; 
-    	tree4.position.z = -20; 
+    // import mesh
+    var dia; 
+    BABYLON.SceneLoader.ImportMesh("", "assets/", "diaMontiffe.babylon", scene, function (newMeshes) {
+    // Set the target of the camera to dia
+    dia = newMeshes[0];
+    camera.target = dia;
+    dia.scaling.x = 0.5;
+    dia.scaling.y = 0.5;
+    dia.scaling.z = 0.5;
+
+    //WORK IN PROGRESS
+    /*
+     if (dia.intersectsMesh(rock, false)) {
+      console.log("COLISSION");
+    } 
+    */
+
+    // Creation of a basic animation with mesh
+    //----------------------------------------
+
+  // Blending animation
+    var animation2Mesh = new BABYLON.Animation("moving", "position.z", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+    BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+                                
+    // Animation keys
+    var keys = [];
+    keys.push({
+        frame: 0,
+        value: -950
+    });
+
+    keys.push({
+        frame: 400,
+        value: 950
+    });
+    animation2Mesh.setKeys(keys);
+  
+    scene.beginDirectAnimation(dia, [animation2Mesh], 0, 400, true);
+    });
+
+    var rock;
+    BABYLON.SceneLoader.ImportMesh("", "assets/", "rock.babylon", scene, function (newMeshes) {
+    rock = newMeshes[0];
+    rock.scaling.x = 10;
+    rock.scaling.y = 10;
+    rock.scaling.z = 10;
+    rock.position.z = -800;
+    });
 
 
-    	return scene;
+    var leafMaterial = new BABYLON.StandardMaterial("leafMaterial", scene);
+    leafMaterial.diffuseColor = new BABYLON.Color3(0.5, 1, 0.5);
+    
+    var woodMaterial = new BABYLON.StandardMaterial(name, scene);
+    woodMaterial.diffuseColor = new BABYLON.Color3(0.627451, 0.321569, 0.176471);
+    
+ 
+    var ground = BABYLON.Mesh.CreatePlane("Plane", 200, scene);
+    ground.rotation.x = glMatrix.toRadian(90);
+    ground.scaling.y = 10;
+    var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
+    groundMaterial.diffuseTexture = new BABYLON.Texture("./textures/snow2.jpg", scene);
+    ground.material = groundMaterial;
+
+    var background = BABYLON.Mesh.CreatePlane("Plane", 100, scene);
+    background.position.x = -100;
+    background.position.y = 50;
+    background.rotation.y = glMatrix.toRadian(-90);
+    background.scaling.x = 20;
+    var backgroundMaterial = new BABYLON.StandardMaterial("background", scene);
+    backgroundMaterial.diffuseTexture = new BABYLON.Texture("./textures/Mountain.jpg", scene);
+    background.material = backgroundMaterial;
+
+    var backgroundRight = BABYLON.Mesh.CreatePlane("Plane", 100, scene);;
+    backgroundRight.position.x = 100;
+    backgroundRight.position.y = 50;
+    backgroundRight.scaling.x = 20;
+    backgroundRight.rotation.y = glMatrix.toRadian(90);
+    backgroundRight.material = backgroundMaterial;
+
+
+    var trees = [];
+    for(var i = 0; i < 50; i++){
+      var tree = QuickTreeGenerator(15, 10, 5, woodMaterial, leafMaterial, scene);
+      tree.position.x = randomNumber(-100, 100);
+      tree.position.z = randomNumber(-1000, 1000);
+      tree.checkCollisions = true;
+      trees.push(tree);
+    }
+
+    window.addEventListener('keydown', function(event) {
+      switch (event.keyCode) {
+        case 37: // Left
+          if(dia.position.x > -90){
+            dia.position.x -= 4;
+            dia.rotation.y = glMatrix.toRadian(-10);
+            window.addEventListener('keyup', function(event) {
+              dia.rotation.y = glMatrix.toRadian(0);
+            });
+          }
+          break;
+
+        case 38: // Up
+        if(dia.position.y < 5){
+          dia.position.y += 5;
+        }
+          break;
+
+        case 39: // Right
+          if(dia.position.x < 90){
+            dia.position.x += 4;
+            dia.rotation.y = glMatrix.toRadian(10);
+            window.addEventListener('keyup', function(event) {
+              dia.rotation.y = glMatrix.toRadian(0);
+            });
+          }
+          break;
+
+        case 40: // Down
+          newAnimation.pause();
+          break;
+      }
+    }, false);
+      return scene;
   };
+
   var scene = createScene();
   engine.runRenderLoop(function(){
-  	scene.render();
+    scene.render();
+    current_frame++;
   });
-
 
   // Watch for browser/canvas resize events
   window.addEventListener("resize", function () {
@@ -154,7 +262,5 @@ function startGame(){
   });
 
 }
-
-
 
 

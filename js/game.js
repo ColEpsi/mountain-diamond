525f5d5ad07b7d39d53966c3dba3a41e7a01dab1
ColEpsi <ugi.vaupotic@gmail.com>
2016-11-09 20:56:02 +0100

    udpate

--- js/game.js
+++ js/game.js
@@ -12,104 +12,19 @@ var randomNumber = function (min, max) {
           }
           var random = Math.random();
           return ((random * (max - min)) + min);
-      };
-var progress = -950;
+};
+
 function startGame(){
   var canvas = document.getElementById("renderCanvas");
   // Load the BABYLON 3D engine
   var engine = new BABYLON.Engine(canvas, true);
+
+
+
   // -------------------------------------------------------------
   // Here begins a function that we will 'call' just after it's built
   
-
-  var createScene = function() {
-    // scene objekt
-    var scene = new BABYLON.Scene(engine);
-    //barva scene
-    scene.clearColor = new BABYLON.Color3( .5, .5, .5);
-
-    // camera
-    var camera = new BABYLON.ArcRotateCamera("camera1",  0, 0, 0, new BABYLON.Vector3(0, 0, 0), scene);
-    camera.setPosition(new BABYLON.Vector3(0, 25, -60));
-    //camera.attachControl(canvas, true);
-    //globalna luc
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 0.5, 0), scene);
-    light.intensity = 0.7;
-    //spot luci
-    var spot = new BABYLON.SpotLight("spot", new BABYLON.Vector3(25, 15, -2000), new BABYLON.Vector3(-1, -0.8, 1), 15, 1, scene);
-    spot.diffuse = new BABYLON.Color3(1, 1, 1);
-    spot.specular = new BABYLON.Color3(0, 0, 0);
-    spot.intensity = 0.8;
-    // import mesh
-    var mesh;
-    BABYLON.SceneLoader.ImportMesh("", "", "assets/diaMontiffe.babylon", scene, function (newMeshes) {
-    
-    // Set the target of the camera to the first imported mesh
-    camera.target = newMeshes[0];
-    mesh = newMeshes[0];
-    newMeshes[0].material = new BABYLON.StandardMaterial("model", scene);
-    newMeshes[0].material.emissiveColor = new BABYLON.Color3(0.2, 0.2, 0.2);
-    newMeshes[0].scaling.x = 0.5;
-    newMeshes[0].scaling.y = 0.5;
-    newMeshes[0].scaling.z = 0.5;
-    //newMeshes[0].position.y = 6;
-    //newMeshes[0].rotation.y = glMatrix.toRadian(180);
-    newMeshes[0].position.z = progress;
-
-
-    // Creation of a basic animation with mesh
-    //----------------------------------------
-
-    var animationMesh = new BABYLON.Animation("moving", "position.z", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-                                                                    BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
-    // Animation keys
-    var keys = [];
-    keys.push({
-        frame: 0,
-        value: -950
-    });
-
-    keys.push({
-        frame: 400,
-        value: 950
-    });
-
-    animationMesh.setKeys(keys);
-
-    mesh.animations.push(animationMesh);
-
-    scene.beginAnimation(mesh, 0, 400, true);
-  
-  // Blending animation
-    var animation2Mesh = new BABYLON.Animation("moving", "position.z", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-  BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
-                                
-  animation2Mesh.enableBlending = true;
-  animation2Mesh.blendingSpeed = 0.01;
-    // Animation keys
-    var keys = [];
-    keys.push({
-        frame: 0,
-        value: -950
-    });
-
-    keys.push({
-        frame: 400,
-        value: 950
-    });
-    animation2Mesh.setKeys(keys);
-  
- 
-    scene.beginDirectAnimation(mesh, [animation2Mesh], 0, 400, true);
-    });
-
-    var leafMaterial = new BABYLON.StandardMaterial("leafMaterial", scene);
-    leafMaterial.diffuseColor = new BABYLON.Color3(0.5, 1, 0.5);
-    
-    var woodMaterial = new BABYLON.StandardMaterial(name, scene);
-    woodMaterial.diffuseColor = new BABYLON.Color3(0.627451, 0.321569, 0.176471);
-    
-   QuickTreeGenerator = function(sizeBranch, sizeTrunk, radius, trunkMaterial, leafMaterial, scene) {
+  QuickTreeGenerator = function(sizeBranch, sizeTrunk, radius, trunkMaterial, leafMaterial, scene) {
 
       var tree = new BABYLON.Mesh("tree", scene);
       tree.isVisible = false;
@@ -188,6 +103,84 @@ function startGame(){
       return tree;
 
   };
+//------------------------------------------------------------------------------------
+
+  var createScene = function() {
+    // scene objekt
+    var scene = new BABYLON.Scene(engine);
+    //barva scene
+    scene.clearColor = new BABYLON.Color3( .5, .5, .5);
+
+    // camera
+    var camera = new BABYLON.ArcRotateCamera("camera1",  0, 0, 0, new BABYLON.Vector3(0, 0, 0), scene);
+    camera.setPosition(new BABYLON.Vector3(0, 25, -60));
+    //camera.attachControl(canvas, true);
+    //globalna luc
+
+    var light0 = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(1, 1, 0), scene);
+    var light1 = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(-1, 1, 0), scene);
+
+    // import mesh
+    var mesh; 
+    BABYLON.SceneLoader.ImportMesh("", "", "assets/diaMontiffe.babylon", scene, function (newMeshes) {
+    
+    // Set the target of the camera to the first imported mesh
+    camera.target = newMeshes[0];
+    mesh = newMeshes[0];
+    mesh.scaling.x = 0.5;
+    mesh.scaling.y = 0.5;
+    mesh.scaling.z = 0.5;
+
+
+    //WORK IN PROGRESS
+    /*
+     if (mesh.intersectsMesh(rock, false)) {
+      console.log("COLISSION");
+    } 
+    */
+
+    // Creation of a basic animation with mesh
+    //----------------------------------------
+
+  // Blending animation
+    var animation2Mesh = new BABYLON.Animation("moving", "position.z", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+    BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+                                
+    // Animation keys
+    var keys = [];
+    keys.push({
+        frame: 0,
+        value: -950
+    });
+
+    keys.push({
+        frame: 400,
+        value: 950
+    });
+    animation2Mesh.setKeys(keys);
+  
+    scene.beginDirectAnimation(mesh, [animation2Mesh], 0, 400, true);
+    });
+
+    var rock;
+    BABYLON.SceneLoader.ImportMesh("", "", "assets/rock.babylon", scene, function (newMeshes) {
+
+    newMeshes[0].scaling.x = 10;
+    newMeshes[0].scaling.y = 10;
+    newMeshes[0].scaling.z = 10;
+    newMeshes[0].position.z = -800;
+    rock = newMeshes[0];
+    });
+
+    
+
+    var leafMaterial = new BABYLON.StandardMaterial("leafMaterial", scene);
+    leafMaterial.diffuseColor = new BABYLON.Color3(0.5, 1, 0.5);
+    
+    var woodMaterial = new BABYLON.StandardMaterial(name, scene);
+    woodMaterial.diffuseColor = new BABYLON.Color3(0.627451, 0.321569, 0.176471);
+    
+ 
     var ground = BABYLON.Mesh.CreatePlane("Plane", 200, scene);
     ground.rotation.x = glMatrix.toRadian(90);
     ground.scaling.y = 10;
@@ -204,6 +197,13 @@ function startGame(){
     backgroundMaterial.diffuseTexture = new BABYLON.Texture("./textures/Mountain.jpg", scene);
     background.material = backgroundMaterial;
 
+    var backgroundRight = BABYLON.Mesh.CreatePlane("Plane", 100, scene);;
+    backgroundRight.position.x = 100;
+    backgroundRight.position.y = 50;
+    backgroundRight.scaling.x = 20;
+    backgroundRight.rotation.y = glMatrix.toRadian(90);
+    backgroundRight.material = backgroundMaterial;
+
 
     var trees = [];
     for(var i = 0; i < 50; i++){
@@ -214,41 +214,28 @@ function startGame(){
       trees.push(tree);
     }
 
-    var tree1 = QuickTreeGenerator(15, 10, 5, woodMaterial, leafMaterial, scene);
-      tree1.position.x = 20;
-
-      var tree2 = QuickTreeGenerator(15, 10, 5, woodMaterial, leafMaterial, scene);
-      tree2.position.x = -20; 
-
-      var tree3 = QuickTreeGenerator(15, 10, 5, woodMaterial, leafMaterial, scene);
-      tree3.position.z = 20;  
-
-      var tree4 = QuickTreeGenerator(15, 10, 5, woodMaterial, leafMaterial, scene);
-      tree4.position.x = 25; 
-      tree4.position.z = -20; 
-
-    scene.collisionsEnabled = true;
-    camera.checkCollisions = true;
-
-    tree1.checkCollisions = true;
-
-
     window.addEventListener('keydown', function(event) {
       switch (event.keyCode) {
         case 37: // Left
-          mesh.position.x -= 3;
+          if(mesh.position.x > -90){
+            mesh.position.x -= 4;
+            mesh.rotation.y = glMatrix.toRadian(-10);
+          }
           break;
 
         case 38: // Up
-          mesh.position.z += 3;
+          mesh.rotation.y = glMatrix.toRadian(0);
           break;
 
         case 39: // Right
-          mesh.position.x += 3;
+          if(mesh.position.x < 90){
+            mesh.position.x += 4;
+            mesh.rotation.y = glMatrix.toRadian(10);
+          }
           break;
 
         case 40: // Down
-          mesh.position.z -= 3;
+          newAnimation.pause();
           break;
       }
     }, false);
@@ -266,7 +253,3 @@ function startGame(){
   });
 
 }
-
-
-
-

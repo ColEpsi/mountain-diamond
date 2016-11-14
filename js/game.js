7b42117bac4c9ca6476868a9293ee1fafda68a38
blazh24 <b.milar@gmail.com>
2016-11-14 22:19:33 +0100

    shit starting to get real
    level design se počasi premika naprej

--- js/game.js
+++ js/game.js
@@ -1,5 +1,4 @@
 // Get the canvas element from our HTML below
-
 document.addEventListener("DOMContentLoaded", function () {
   if (BABYLON.Engine.isSupported()) {
     startGame();
@@ -20,9 +19,32 @@ function startGame(){
   var engine = new BABYLON.Engine(canvas, true);
 
   //global variables
-  var dia;
-  var rock;
+  var dia, rock, snowPile, snowman; // MESHES
   var trees = [];
+  var rocks = [];
+  var snowPiles = [];
+  var snowmen = [];
+  var slope = 5;
+  var length = 5000, width = 500;
+  var originalSpeed = 3;
+  var speed = originalSpeed, gravity = 0.20, steeringFactor = 1; // variables for movement
+  // OBSTACLE POSITIONS
+  	// ROCKS
+  	var rockPositionsX = [-15, -30, -40, -80, -81, -85, -90, -102, 15, 38, 50, 200];
+  	var rockPositionsZ = [-2400, -2200, -2, -790, -900, -1300, -10, -1111, -1239, -2000, -1530, -1450];
+	var rockCtr = 0;
+	// SNOW PILES
+  	var snowPilePositionsX = [175, 20, -50, -155, 250];
+  	var snowPilePositionsZ = [-2000, -1120, 50, 700, 1200];
+	var snowPileCtr = 0;
+  	// TREES
+	var treePositionsX = [];
+	var treePositionsZ = [];
+	var treeCtr = 0;
+	// SNOWMEN
+	var snowmanPositionsX = [];
+	var snowmanPositionsZ = [];
+	var snowmanCtr = 0;
   // -------------------------------------------------------------
   // Here begins a function that we will 'call' just after it's built
   
@@ -38,30 +60,66 @@ function startGame(){
     // camera
     var camera = new BABYLON.ArcRotateCamera("camera1",  0, 0, 0, new BABYLON.Vector3(0, 0, 0), scene);
     camera.setPosition(new BABYLON.Vector3(0, 25, -60)); //0, 50, -100?
-    //camera.attachControl(canvas, true);
+    camera.attachControl(canvas, true);
+    
     //globalna luc
-
     var light0 = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(1, 1, 0), scene);
     var light1 = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(-1, 1, 0), scene);
 
-    // import mesh --> GLAVNI MESH = dia
-    
+    // MESH IMPORTS
     BABYLON.SceneLoader.ImportMesh("", "", "assets/diaMontiffe.babylon", scene, function (newMeshes) {
         camera.target = newMeshes[0];
         dia = newMeshes[0];
         dia.scaling.x = 0.5;
         dia.scaling.y = 0.5;
         dia.scaling.z = 0.5;
-        dia.position.z = -950;
+        dia.position.z = -length/2 ;//+ 50;
+		dia.position.y = -dia.position.z * glMatrix.toRadian(slope);
     });
-  
-    BABYLON.SceneLoader.ImportMesh("", "", "assets/rock.babylon", scene, function (newMeshes) {
-      newMeshes[0].scaling.x = 10;
-      newMeshes[0].scaling.y = 10;
-      newMeshes[0].scaling.z = 10;
-      newMeshes[0].position.z = -800;
+
+  for(var j = 0; j < rockPositionsX.length; j++){
+    BABYLON.SceneLoader.ImportMesh("", "", "assets/cube.babylon", scene, function (newMeshes) { //rock zahteven, začasno cube
       rock = newMeshes[0];
+      rock.scaling.x = 5;
+      rock.scaling.y = 5;
+      rock.scaling.z = 5;
+
+      rock.position.x = rockPositionsX[rockCtr]; // randomNumber(-400, 400); // ne dela, piše da je undefined o.O
+     	rock.position.z = rockPositionsZ[rockCtr]; //randomNumber(-length/2, length/2);
+     	console.log("narjen kamen " + rockCtr + " na " + rock.position.x +", " + rock.position.z);
+			rock.position.y = -rock.position.z * glMatrix.toRadian(slope);
+			rock.rotation.x = glMatrix.toRadian(-slope);
+      rocks.push(rock);
+	  rockCtr++;
     });
+  }
+
+    for(var j = 0; j < snowPilePositionsX.length; j++){
+	  BABYLON.SceneLoader.ImportMesh("", "", "assets/mountain-rock.babylon", scene, function (newMeshes) {
+	    snowPile = newMeshes[0];
+	    snowPile.scaling.x = 90;
+	    snowPile.scaling.y = 90;
+	    snowPile.scaling.z = 90;
+	    snowPile.position.x = snowPilePositionsX[snowPileCtr];//+ 50;
+	    snowPile.position.z = snowPilePositionsZ[snowPileCtr];//+ 50;
+		snowPile.position.y = -snowPile.position.z * glMatrix.toRadian(slope) - 50; //-50 ker je ta objekt ogromen in dela probleme s collisions
+		snowPiles.push(snowPile);
+		snowPileCtr++;
+	  });
+	}
+
+	//for(var j = 0; j < snowPilePositionsX.length; j++){
+	  BABYLON.SceneLoader.ImportMesh("", "", "assets/snowmanstl.babylon", scene, function (newMeshes) {
+	    snowman = newMeshes[0];
+	    snowman.scaling.x = 0.4;
+	    snowman.scaling.y = 0.4;
+	    snowman.scaling.z = 0.4;
+	    snowman.position.x = 0;//+ 50;
+	    snowman.position.z = -2300;//+ 50;
+		snowman.position.y = (-snowman.position.z * glMatrix.toRadian(slope)) + 17; // + 17 ker je testen snežak ogromen
+		snowmanCtr++;
+	  });
+	//}
 
     var leafMaterial = new BABYLON.StandardMaterial("leafMaterial", scene);
     leafMaterial.diffuseColor = new BABYLON.Color3(0.5, 1, 0.5);
@@ -69,26 +127,30 @@ function startGame(){
     var woodMaterial = new BABYLON.StandardMaterial(name, scene);
     woodMaterial.diffuseColor = new BABYLON.Color3(0.627451, 0.321569, 0.176471);
 
-    var ground = BABYLON.Mesh.CreateGround("ground1", 800, 2000, 2, scene);
+    //var ground = BABYLON.Mesh.CreateGround("ground1", 800, 2000, 2, scene);
     var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
     groundMaterial.diffuseTexture = new BABYLON.Texture("./textures/snow2.jpg", scene);
-    var ghm = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "lightning.png", 800, 2000, 3, 0.1, 0.5, scene, false); //ime, url, width, height, subdivizije, minH, maxH 
+    var ghm = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "lightning.png", width, length, 3, 0.1, 0.5, scene, false); //ime, url, width, height, subdivizije, minH, maxH 
     ghm.material = groundMaterial;
+	ghm.rotation.x = glMatrix.toRadian(slope);
 
+	// CANVAS EDGES (left, right, bottom)
     var leftWall = BABYLON.Mesh.CreatePlane("Plane", 100, scene);
-    leftWall.position.x = -400;
+    leftWall.position.x = -width/2;
     leftWall.position.y = 50;
     leftWall.rotation.y = glMatrix.toRadian(-90);
-    leftWall.scaling.x = 20;
+    leftWall.rotation.z = glMatrix.toRadian(-slope);
+    leftWall.scaling.x = 100;
     var leftWallMaterial = new BABYLON.StandardMaterial("background", scene);
     leftWallMaterial.diffuseTexture = new BABYLON.Texture("./textures/Mountain.jpg", scene);
     leftWall.material = leftWallMaterial;
 
     var rightWall = BABYLON.Mesh.CreatePlane("Plane", 100, scene);;
-    rightWall.position.x = 400;
+    rightWall.position.x = width/2;
     rightWall.position.y = 50;
-    rightWall.scaling.x = 20;
+    rightWall.scaling.x = 100;
     rightWall.rotation.y = glMatrix.toRadian(90);
+    rightWall.rotation.z = glMatrix.toRadian(slope);
     rightWall.material = leftWallMaterial;
 
     /*
@@ -110,8 +172,10 @@ function startGame(){
     
     for(var i = 0; i < 50; i++){
       var tree = QuickTreeGenerator(15, 10, 5, woodMaterial, leafMaterial, scene);
-      tree.position.x = randomNumber(-100, 100);
-      tree.position.z = randomNumber(-1000, 1000);
+      tree.position.x = randomNumber(-400, 400);
+      tree.position.z = randomNumber(-length/2, length/2);
+			tree.position.y = -tree.position.z * glMatrix.toRadian(slope);
+			tree.rotation.x = glMatrix.toRadian(-slope);
       trees.push(tree);
     }
       return scene;
@@ -132,6 +196,10 @@ function startGame(){
           moveRight = true;
           dia.rotation.y = glMatrix.toRadian(10);
           break;
+
+        case 40:
+        case 83:
+        	speed = originalSpeed/2;
         }
     }, false);
 
@@ -139,27 +207,30 @@ function startGame(){
       moveLeft = false;
       moveRight = false;
       dia.rotation.y = glMatrix.toRadian(0);
-
+      speed = originalSpeed; // treba poslušat izrecno za tipko S / down arrow
     }, false);
 
   //ANIMATION
-  var speed = 3.0, gravity = 0.20;
   var scene = createScene();
   engine.runRenderLoop(function(){
+	
       if(scene.isReady()){
-         if (dia.position.z > 950){
-           dia.position = new BABYLON.Vector3(0, 0, -950);
+         if (dia.position.z > (length/2)-50){ // GAME LOOP
+           dia.position = new BABYLON.Vector3(0, 0, (-length/2)+50);
          }
-         if (dia.position.z < 950){
+         //if (dia.position.z < 950){
            dia.position.z += speed;
-           dia.rotation.x = glMatrix.toRadian(-4);
-         }
+		   dia.position.y = -dia.position.z * glMatrix.toRadian(slope);
+           dia.rotation.x = glMatrix.toRadian(slope);
+         //}
          if (moveRight){
-           dia.position.x += 1;
+           if(speed != originalSpeed) dia.position.x += steeringFactor/2;
+           else dia.position.x += steeringFactor;
            //dia.moveWithCollisions(3, 0, 0); //metoda sprejme vektor
          }
          if (moveLeft){
-           dia.position.x -= 1;
+           if(speed != originalSpeed) dia.position.x -= steeringFactor/2;
+           else dia.position.x -= steeringFactor;
            //dia.moveWithCollisions(speed, gravity, 0); //metoda sprejme vektor
          }
          trees.forEach(function(tree){
@@ -171,13 +242,26 @@ function startGame(){
             }
           } 
          });
-         if(dia.intersectsMesh(rock, false)){
+
+         rocks.forEach(function(rock){
+         		if(dia.intersectsMesh(rock, false)){
             console.warn("collision!!");
             engine.stopRenderLoop();
             if(window.confirm("GAME OVER\nPLAY AGAIN?") == true){
               startGame();
             }
-         } 
+          }
+         });
+
+         snowPiles.forEach(function(snowPile){
+         	if(dia.intersectsMesh(snowPile, false)){
+            console.warn("collision!!");
+            engine.stopRenderLoop();
+            if(window.confirm("GAME OVER\nPLAY AGAIN?") == true){
+              startGame();
+            }
+          }
+         });
       }
       scene.render();
   });
